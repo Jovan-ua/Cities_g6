8b976a79312c290a095bdc832b918915e01a4ee6
VVVladyslav <vladoszhar@gmail.com>
2023-10-22 22:29:35 +0300

    Код с заглушками под UI

--- src/main/java/org/example/Logic.java
+++ src/main/java/org/example/Logic.java
@@ -1,5 +1,117 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
+// Для ментора.  Не хочу писать else if() для более легкой прочитки кода
 public class Logic {
-    //qq
+    private int ReturnCode = 0;
+    ArrayList<String> ListOfCity;
+    ArrayList<String> ListOfUsedCity;
+    ArrayList<String> ListOfAllCity;
+    private int score;
+
+    public Logic() {
+        ListOfCity = new ArrayList<>(List.of(new String[]{"Cameroon", "Solomon Islands", "Mamoa", "Sierra Leone"})); // Города с которыми мы играем
+        ListOfUsedCity = new ArrayList<>(); // Место где храним использованые города за всю игру
+        ListOfAllCity = new ArrayList<>(List.of(new String[]{"Kiev", "Cameroon", "Mamoa", "Sierra Leone"})); // Все города мира чтобы делать проверку на User написал правильный город мира
+        score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getReturnCode() {
+        return ReturnCode;
+    }
+
+    public void setReturnCode(int returnCode) {
+        ReturnCode = returnCode;
+    }
+
+    public void SortWordUser(String WordUser) {
+        setReturnCode(0);
+
+        /*
+        1 Сделать counter
+        2 сделать Логику компьютера
+        3 Сменить города на украинский текст
+
+        *Дополнительно можно сделать ArrayList в котором будут города под рандомными индексами
+        */
+
+        //setReturnCode(Число); Используем для того чтобы понять что пошло не так или так
+
+        // 1 Проверяем длинну; - Если слово состоит из малого количества букв = Код возврата = 1
+        // 2 НЕ = ли здаюсь; - Если слово здаюсь - Код возврата = 2
+        // 3 Не в списке использованых; - Слово было использовано - Код возврата = 3
+        // 4 а город ли это; Игрок ввел НЕ город - Код возврата = 4
+        // 5 1-буква введенного города не= последней букве ранее введенного; 1 буква слова что ввел игрок НЕ соответствует последней букве компьютера - Код возврата = 5
+        // 6 добавление всего в случае прохождения проверок; Все проверки пройдены игра продолжаеться - Код возврата = 0
+        // 7 Компьютер сдался
+
+        // 1 Проверяем длинну
+        if (WordUser.length() < 3) {
+            this.setReturnCode(1);
+
+        }
+        // 2 НЕ = ли слово здаюсь
+        if (WordUser.equals("здаюсь") && getReturnCode() == 0) {
+            this.setReturnCode(2);
+
+        }
+        // 3 Не в списке использованых
+        if (this.ListOfUsedCity.contains(WordUser)) {
+            this.setReturnCode(3);
+
+        }
+        // 4 А город ли это во всем мире
+        if (this.ListOfUsedCity.size() > 0 && this.ListOfUsedCity.contains(WordUser) && getReturnCode() == 0) {
+            this.setReturnCode(4);
+
+        }
+        // 5 1-буква введенного города не= последней букве ранее введенного
+        if (this.ListOfUsedCity.size() > 0 && getReturnCode() == 0) {
+            String LastWord = String.valueOf(ListOfUsedCity.size() - 1);
+            String LastLetter = String.valueOf(LastWord.charAt(LastWord.length() - 1));
+            String FirstLatter = String.valueOf(WordUser.charAt(0));
+
+            if (LastLetter != FirstLatter && getReturnCode() == 0) {
+                this.setReturnCode(5);
+
+            }
+        }
+        // 6 добавление всего в случае прохождения всех проверок
+        if (ReturnCode == 0 && getReturnCode() == 0) {
+            ListOfUsedCity.add(WordUser);
+            ListOfCity.remove(WordUser);
+
+            StepComputer();
+        }
+
+
+        // 7
+//        if (ListOfCity.size() == 0){
+//            setReturnCode(7);
+//        }
+    }
+
+    public void StepComputer() {
+        String LastWord = String.valueOf(this.ListOfUsedCity.size() - 1);
+        String founder = String.valueOf(LastWord.charAt(LastWord.length() - 1));
+
+        for (String wordFromList : ListOfCity) {
+            if (wordFromList.contains(founder)) {
+                System.out.println("Computer said + " + wordFromList);
+                ListOfUsedCity.add(wordFromList);
+                ListOfCity.remove(wordFromList);
+                break;
+            }
+        }
+    }
+
+    public String ReturnLastWord(){
+        String LastWord = String.valueOf(ListOfUsedCity.size() - 1); // Слово что вывел компьютер
+        return LastWord;
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,64 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    public static Logic gameLogica;
+    public static JTextField textField;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+//Костыль для UI
+
+        gameLogica = new Logic();
+        JFrame frame = new JFrame("Окно с полем ввода");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        textField = new JTextField(20);
+        JButton button = new JButton("Нажми меня");
+        panel.add(textField);
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainLogic();
+            }
+        });
+        frame.add(panel);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+    }
+
+    public static void MainLogic() {
+        String inputText = textField.getText(); // Получаем текст из поля ввода
+        gameLogica.SortWordUser(inputText);
+        int returncode = gameLogica.getReturnCode();
+
+        gameLogica.getScore();
+        if (returncode == 0) {
+            System.out.println("all good");
+            textField.setText("");
+        } else if (returncode == 1) {
+            System.out.println("word less than 3 latters");
+            textField.setText("");
+        } else if (returncode == 2) {
+            System.out.println("you write здаюсь ");
+            textField.setText("");
+        } else if (returncode == 4) {
+            System.out.println("word is used");
+            textField.setText("");
+        } else if (returncode == 5) {
+            System.out.println("Later is not correct. last word ended on the auther later");
+            textField.setText("");
+        } else if (returncode == 6) {
+            System.out.println("this is not a city from world");
+            textField.setText("");
+        } else if (returncode == 6) {
+            System.out.println("Computer loser");
+            textField.setText("");
+        }
     }
 }
